a5b8f6451096693fe0cc9c1a9e7007f4a8bb2c1b
mellopes06br <163359225+mellopes06br@users.noreply.github.com>
2025-08-30 15:12:49 +0000

    criando teste

--- src/test/java/BancoTest.java
+++ src/test/java/BancoTest.java
@@ -1,6 +1,52 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.function.BooleanSupplier;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import com.example.Banco;
 
 public class BancoTest {
 
+    Banco b;
+
+   @BeforeEach
+    public void setUp(){
+        b = new Banco("666", 1000);
+    }
+
+    //se você declara no before each a conta, ele ja fica criado para todos os testes, pra 
+    //não ter que criar sempre um objeto novo  
+
+    @Test
+    public void saldoNovaContaTest (){
+       //Banco b = new Banco("666", 1000);
+        assertEquals(1000, b.getBalance());   // comparando o mil com o dinheiro da conta
+    }
     
+    @Test
+    public void depositoValidoTest(){
+       //Banco b = new Banco("666", 1000);
+        b.deposit(500); 
+        assertEquals(1500, b.getBalance());  //comparando o valor esperado com o resultado 
+    }
+
+    @Test
+    public void depoistoInvalidoTest(){
+    // Banco b = new Banco("666", 1000);
+        b.deposit(-100);
+        assertEquals(1000, b.getBalance());
+    }
+
+    @Test
+    public void saqueValidotest(){
+        boolean ret = b.withdraw(100);  
+        assertTrue(ret);
+        assertEquals(900, b.getBalance());
+    }
+
+    // é usado o assert true pra saber se foi permitido e o
+    // equals é para saber se o saque funcionou.
 }
